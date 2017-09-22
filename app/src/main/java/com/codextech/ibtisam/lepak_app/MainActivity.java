7ce4db511a9a081b59fab3c963adac3fbd68f93b
IBTISAM-PC\ibtisam <ibtisam.asif@codextech.com>
2017-09-22 14:28:13 -0700

    summary
    test comit to master

---
+++ app/src/main/java/com/codextech/ibtisam/lepak_app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.codextech.ibtisam.lepak_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("a", "onCreate: ");
+        Log.d("a", "onCreate: ");
+        Log.d("a", "onCreate: ");
+        Log.d("a", "onCreate: ");
+
+    }
+}
