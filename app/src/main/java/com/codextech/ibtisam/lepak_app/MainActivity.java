1a43b1f601660065c94dba8c4a26c4679b2414c2
IBTISAM-PC\ibtisam <ibtisam.asif@codextech.com>
2017-09-22 22:29:17 -0700

    ibtisam-pc commit
    ibtisam-pc commit descriotionfckmsnvls

--- app/src/main/java/com/codextech/ibtisam/lepak_app/MainActivity.java
+++ app/src/main/java/com/codextech/ibtisam/lepak_app/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("a", "onCreate: ");
         Log.d("a", "commit from 4540s");
         Log.d("a", "commit from Touqeer");
+        Log.d("a", "commit from ibtisam-pc");
 
     }
 }
