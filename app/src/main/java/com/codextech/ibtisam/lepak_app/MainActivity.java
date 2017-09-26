98b6cab1f808900316f1247fc842319a5a68b3e6
IBTISAM-PC\ibtisam <ibtisam.asif@codextech.com>
2017-09-26 08:13:22 +0500

    ignore commit

--- app/src/main/java/com/codextech/ibtisam/lepak_app/MainActivity.java
+++ app/src/main/java/com/codextech/ibtisam/lepak_app/MainActivity.java
@@ -2,6 +2,7 @@ package com.codextech.ibtisam.lepak_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
                    // intent.putExtra(EXTRA,message);
                    // startActivity(intent);
+                    Log.d("a", "onClick: ");
 
                 }
             });
