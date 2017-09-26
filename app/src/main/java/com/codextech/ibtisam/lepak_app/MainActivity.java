e103a850eca352618d51f540cca7ff7df3ce3a59
IBTISAM-PC\ibtisam <ibtisam.asif@codextech.com>
2017-09-27 00:31:05 +0500

    successful removal of idea and other useless files in commit

--- app/src/main/java/com/codextech/ibtisam/lepak_app/MainActivity.java
+++ app/src/main/java/com/codextech/ibtisam/lepak_app/MainActivity.java
@@ -33,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
 
                    // intent.putExtra(EXTRA,message);
                    // startActivity(intent);
-                    Log.d("a", "onClick: ");
-
                 }
             });
 
